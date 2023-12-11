e61c2d75f560c664e2e3b983c40699cb0b4abaa5
Radu-Augustin Vele <vele.ne.radu@student.utcluj.ro>
2023-12-11 11:16:44 +0200

    use the right version of stomp docs: https://stomp-js.github.io/guide/stompjs/using-stompjs-v5.html

--- energy-ms/src/components/system/ChatBox.tsx
+++ energy-ms/src/components/system/ChatBox.tsx
@@ -1,9 +1,10 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import { CHAT_USERVICE_WEBSOCKET } from "../../constants/WebSocketConstants";
-import Stomp from 'stompjs';
 import { ChatBoxProps } from "../../types/ChatBoxProps";
 import { UserChatService } from "../../api/users/UserChatService";
+import { Client, Message } from '@stomp/stompjs';
+import { Frame } from "stompjs";
 
 export default function ChatBox(props: ChatBoxProps): React.JSX.Element {
     const stompClientRef = useRef(null);
@@ -12,16 +13,31 @@ export default function ChatBox(props: ChatBoxProps): React.JSX.Element {
     const [isChatInit, setIsChatInit] = useState(false)
     const [pairedAdmin, setPairedAdmin] = useState(null) // for user
     const [pairedUserList, setPairedUserList] = useState(null) // for admin
+    const [currentlyWrittenMessage, setCurrentlyWrittenMessage] = useState('')
 
-    const onMessageReceive = (message) => {
-        console.log("Received message");
+    const onMessageReceived = (payload) => {
+        console.log("Received message", payload);
     }
 
-    const onConnect = (stomp) => {
-        // reference the stomp client connection
-        stompClientRef.current = stomp;
+    const onConnect = (frame: Frame) => {
+        console.log("Client connected to websocket: ", frame)
+        // // reference the stomp client connection
+        if (stompClientRef.current) {
+            let stomp = stompClientRef.current;
+            stomp.subscribe(`/queue/${sessionStorage.getItem("authUser")}/messages`, onMessageReceived)
+            console.log("Successfully subscribed to its own queue")
+        }
+        else {
+            console.log("Messed up, bro")
+        }
 
-        // subscribe to its own queue
+        // // subscribe to its own queue
+        // //stomp.subscribe(`/queue/${sessionStorage.getItem("authUser")}/messages`, onMessageReceived);
+        // console.log("successfully subscribed to its own queue")
+    }
+
+    const onError = () => {
+        console.log("Error encountered while connecting to ws");
     }
 
     const socketOpen = () => {
@@ -29,10 +45,18 @@ export default function ChatBox(props: ChatBoxProps): React.JSX.Element {
             stompClientRef.current.disconnect();
         }
 
-        let stomp = Stomp.client(`${CHAT_USERVICE_WEBSOCKET}?name=${sessionStorage.getItem("authUser")}&role=${sessionStorage.getItem("role")}`);
-        stomp.connect({
-            Authorization: `${localStorage.getItem("token")}`
-        }, onConnect(stomp));
+        let stomp = new Client({  
+            brokerURL: `${CHAT_USERVICE_WEBSOCKET}?name=${sessionStorage.getItem("authUser")}&role=${sessionStorage.getItem("role")}`,
+            connectHeaders: {
+                login: 'dummy',
+                passcode: 'dummy',
+            },
+            reconnectDelay: 5000,
+        }); 
+
+        stomp.onConnect = onConnect
+        stomp.activate();        
+        stompClientRef.current = stomp;
     }
 
     useEffect(() => {
@@ -42,7 +66,7 @@ export default function ChatBox(props: ChatBoxProps): React.JSX.Element {
 
         return () => {
             if (stompClientRef.current) {
-                stompClientRef.current.disconnect();
+                stompClientRef.current.deactivate();
             }
         }
     }, [])
@@ -67,16 +91,38 @@ export default function ChatBox(props: ChatBoxProps): React.JSX.Element {
         }
         else {
             if (stompClientRef.current && props.isUser) {
-                stompClientRef.current.disconnect();
+                stompClientRef.current.deactivate();
                 setPairedAdmin(null);
             }
         }
     }, [isChatInit])    
 
+    const sendMessage = (destinationEmail) => {
+        if (stompClientRef.current && destinationEmail !== '') {
+            let message = {
+                senderEmailAddress: sessionStorage.getItem("authUser"),
+                receiverEmailAddress: destinationEmail,
+                messageBody: currentlyWrittenMessage,
+                timestamp: new Date()
+            }
+            //stompClientRef.current.send("/app/chat");
+            setCurrentlyWrittenMessage("");
+        }
+    }
+
     return (
         <Box>
             {props.isUser ? 
-            <Button onClick={() => setIsChatInit(!isChatInit)} variant="contained"> Toggle Chat </Button> 
+            <>
+                <Button onClick={() => setIsChatInit(!isChatInit)} variant="contained"> Toggle Chat </Button> 
+                <TextField
+                    label="message"
+                    value={currentlyWrittenMessage}
+                    onChange={(e) => setCurrentlyWrittenMessage(e.target.value)}
+                    fullWidth
+                />                    
+                <Button variant="contained" onClick={() => sendMessage(pairedAdmin)}>Send message</Button>
+            </>
             : <></>}
         </Box>
     )
